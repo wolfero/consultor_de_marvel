601ba74a3ab31ca5ccc83438ac7635720d003c58
Wolfero <ferorock44@gmail.com>
2023-03-04 21:20:16 +0000

    💄 updated cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -6,10 +6,17 @@ import {
 	Heading,
 	Image,
 	Stack,
-	Card as Cart,
+	Card,
 	Text,
+	useDisclosure,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
+	VStack,
 } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
 import { Character } from '../../types/character';
 import { Comic } from '../../types/comic';
 import { Series } from '../../types/serie';
@@ -21,29 +28,52 @@ interface CardProps {
 export const CharacterCards = ({ character }: CardProps) => {
 	const { id, name, description, thumbnail } = character;
 	const imageUrl = `${thumbnail.path}.${thumbnail.extension}`;
-	const navigate = useNavigate();
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
-		<Cart maxW="sm">
+		<Card maxW="sm">
 			<CardBody>
-				<Image src={imageUrl} alt={name} borderRadius="lg" />
+				<Image src={imageUrl} alt={name} borderRadius="lg"/>
 				<Stack mt="6" spacing="3">
 					<Heading size="md">{name}</Heading>
-					<Text>{description === '' ? 'Description not available' : description}</Text>
+					<Text>{!description ? 'Description not available' : description}</Text>
 				</Stack>
 			</CardBody>
 			<Divider />
 			<CardFooter justifyContent="center">
-				<Button
-					variant="solid"
-					bg="red.600"
-					_hover={{ bg: 'blue.500' }}
-					onClick={() => navigate(`${id}`)}
-				>
+				<Button variant="solid" bg="red.600" _hover={{ bg: 'blue.500' }} onClick={onOpen}>
 					Details
 				</Button>
+
+				<Modal
+					blockScrollOnMount={false}
+					size={'2xl'}
+					isOpen={isOpen}
+					isCentered
+					onClose={onClose}
+				>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader>{name}</ModalHeader>
+						<ModalCloseButton />
+						<ModalBody>
+							<VStack p={5} spacing="3">
+								<Image
+									boxSize="md"
+									objectFit="contain"
+									src={imageUrl}
+									alt={name}
+									borderRadius="lg"
+								/>
+								<Text>
+									{!description ? 'Description not available' : description}
+								</Text>
+							</VStack>
+						</ModalBody>
+					</ModalContent>
+				</Modal>
 			</CardFooter>
-		</Cart>
+		</Card>
 	);
 };
 
@@ -54,29 +84,52 @@ interface ComicProps {
 export const ComicsCards = ({ comic }: ComicProps) => {
 	const { id, title, description, thumbnail } = comic;
 	const imageUrl = `${thumbnail.path}.${thumbnail.extension}`;
-	const navigate = useNavigate();
+	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
-		<Cart maxW="sm">
+		<Card maxW="sm">
 			<CardBody>
 				<Image src={imageUrl} alt={title} borderRadius="lg" />
 				<Stack mt="6" spacing="3">
 					<Heading size="md">{title}</Heading>
-					<Text>{description === '' ? 'Description not available' : description}</Text>
+					<Text>{!description ? 'Description not available' : description}</Text>
 				</Stack>
 			</CardBody>
 			<Divider />
 			<CardFooter justifyContent="center">
-				<Button
-					variant="solid"
-					bg="red.600"
-					_hover={{ bg: 'blue.500' }}
-					onClick={() => navigate(`${id}`)}
-				>
+				<Button variant="solid" bg="red.600" _hover={{ bg: 'blue.500' }} onClick={onOpen}>
 					Details
 				</Button>
+
+				<Modal
+					blockScrollOnMount={false}
+					size={'2xl'}
+					isOpen={isOpen}
+					isCentered
+					onClose={onClose}
+				>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader>{title}</ModalHeader>
+						<ModalCloseButton />
+						<ModalBody>
+							<VStack p={5} spacing="3">
+								<Image
+									boxSize="md"
+									objectFit="contain"
+									src={imageUrl}
+									alt={title}
+									borderRadius="lg"
+								/>
+								<Text>
+									{!description ? 'Description not available' : description}
+								</Text>
+							</VStack>
+						</ModalBody>
+					</ModalContent>
+				</Modal>
 			</CardFooter>
-		</Cart>
+		</Card>
 	);
 };
 
@@ -87,28 +140,53 @@ interface SeriesProps {
 export const SeriesCards = ({ serie }: SeriesProps) => {
 	const { id, title, description, thumbnail } = serie;
 	const imageUrl = `${thumbnail.path}.${thumbnail.extension}`;
-	const navigate = useNavigate();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	console.log(description);
 
 	return (
-		<Cart maxW="sm">
+		<Card maxW="sm">
 			<CardBody>
 				<Image src={imageUrl} alt={title} borderRadius="lg" />
 				<Stack mt="6" spacing="3">
 					<Heading size="md">{title}</Heading>
-					<Text>{description === '' ? 'Description not available' : description}</Text>
+					<Text>{!description ? 'Description not available' : description}</Text>
 				</Stack>
 			</CardBody>
 			<Divider />
 			<CardFooter justifyContent="center">
-				<Button
-					variant="solid"
-					bg="red.600"
-					_hover={{ bg: 'blue.500' }}
-					onClick={() => navigate(`${id}`)}
-				>
+				<Button variant="solid" bg="red.600" _hover={{ bg: 'blue.500' }} onClick={onOpen}>
 					Details
 				</Button>
+
+				<Modal
+					blockScrollOnMount={false}
+					size={'2xl'}
+					isOpen={isOpen}
+					isCentered
+					onClose={onClose}
+				>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader>{title}</ModalHeader>
+						<ModalCloseButton />
+						<ModalBody>
+							<VStack p={5} spacing="3">
+								<Image
+									boxSize="md"
+									objectFit="contain"
+									src={imageUrl}
+									alt={title}
+									borderRadius="lg"
+								/>
+								<Text>
+									{!description ? 'Description not available' : description}
+								</Text>
+							</VStack>
+						</ModalBody>
+					</ModalContent>
+				</Modal>
 			</CardFooter>
-		</Cart>
+		</Card>
 	);
 };
