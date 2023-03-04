90a11d5e6798388b954aee91cec7f254c65ff5c4
Wolfero <ferorock44@gmail.com>
2023-03-04 21:22:30 +0000

    💄 updated main page style

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,12 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
 
-const Main = () => <Box minH={'90vh'}>Welcome in Marvel Universe</Box>;
+const Main = () => (
+	<Box minH={'90vh'}>
+		<Flex justifyContent={'center'}>
+			<Image src="/logo.svg" boxSize={'sm'} objectFit="contain" alt="Logo Marvel" />
+		</Flex>
+		<Heading size={'4xl'} textAlign={'center'}>CONSULTANT</Heading>
+	</Box>
+);
 
 export default Main;
