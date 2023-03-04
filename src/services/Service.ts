f1ea73bce6bcd688b0e9e64746799df910622b55
Wolfero <ferorock44@gmail.com>
2023-03-04 09:28:28 +0000

    ✨ Added service to get data from API

---
+++ src/services/Service.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import md5 from 'md5';
+import { CharacterDataWrapper } from '../types/character';
+import { ComicDataWrapper } from '../types/comic';
+import { SeriesDataWrapper } from '../types/serie';
+
+const apiPublicKey = process.env.REACT_APP_PUBLIC_KEY;
+const apiPrivateKey = process.env.REACT_APP_PRIVATE_KEY;
+const ts = Number(new Date());
+const hash = md5(`${ts}${apiPrivateKey}${apiPublicKey}`);
+const urlApi = 'http://gateway.marvel.com/v1/public/';
+
+export const getCharactersData = async () => {
+	const url = `${urlApi}characters?ts=${ts}&apikey=${apiPublicKey}&hash=${hash}`;
+	const response = await axios.get(url);
+	const characterData: CharacterDataWrapper = response.data;
+	return characterData;
+};
+
+export const getComicsData = async () => {
+	const url = `${urlApi}comics?ts=${ts}&apikey=${apiPublicKey}&hash=${hash}`;
+	const response = await axios.get(url);
+	const comicData: ComicDataWrapper = response.data;
+	return comicData;
+};
+
+export const getSeriesData = async () => {
+	const url = `${urlApi}series?ts=${ts}&apikey=${apiPublicKey}&hash=${hash}`;
+	const response = await axios.get(url);
+	const seriesData: SeriesDataWrapper = response.data;
+	return seriesData;
+};
