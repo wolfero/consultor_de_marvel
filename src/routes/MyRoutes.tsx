72380817099cbd28d8e1aeda2e067faabd1f4a76
Wolfero <ferorock44@gmail.com>
2023-03-05 17:54:30 +0000

    🚚 Update routes

--- src/routes/MyRoutes.tsx
+++ src/routes/MyRoutes.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import Characters from '../components/Characters/Characters';
 import GrilleCharacters from '../components/GrilleCharacters/GrilleCharacters';
 import GrilleComics from '../components/GrilleComics/GrilleComics';
 import GrilleSeries from '../components/GrilleSeries/GrilleSeries';
@@ -10,7 +9,6 @@ function MyRoutes() {
 		<Routes>
 			<Route path="/" element={<Main />} />
 			<Route path="/characters" element={<GrilleCharacters />} />
-			<Route path="/characters/:id" element={<Characters />} />
 			<Route path="/comics" element={<GrilleComics />} />
 			<Route path="/series" element={<GrilleSeries />} />
 		</Routes>
