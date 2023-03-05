9cd3ca2d447711f51b596f41e8b5997e6b065dde
Wolfero <ferorock44@gmail.com>
2023-03-05 15:33:32 +0000

    ✨ Added Characters pagination

--- src/components/GrilleCharacters/GrilleCharacters.tsx
+++ src/components/GrilleCharacters/GrilleCharacters.tsx
@@ -1,38 +1,97 @@
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
+import { Box, Flex, SimpleGrid } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import useSection from '../../hooks/useSection';
+import { MarvelService } from '../../services/MarvelService';
+import { CharactersDataContainer, CharactersDataWrapper } from '../../types/characters';
+import { SectionContextType } from '../../types/section';
 import { CharacterCards } from '../Cards/Cards';
 import { ErrorAlert } from '../ErrorAlert/ErrorAlert';
 import { Spinner } from '../Spinner/Spinner';
 
-const GrilleCharacters = () => {
-	const { characters, error } = useSection();
+import styles from '../../styles/global.module.scss';
 
-	console.log(characters?.data.results);
+const GrilleCharacters = () => {
+	const { data, updateContextData } = useSection();
+	const [pageCount, setPageCount] = useState(0);
+	const numberOfCharactersInPage = 20;
+	const [charactersData, setCharactersData] = useState<CharactersDataContainer | null>(null);
+
+	const handleClick = async (newPageCount: number) => {
+		const marvelService = new MarvelService();
+		const newCharacters = await marvelService.getNextCharactersData(newPageCount);
+		const aux = updateData(newCharacters);
+		updateContextData(aux);
+	};
+
+	const updateData = (newCharacters: CharactersDataWrapper): SectionContextType => {
+		return {
+			characters: newCharacters,
+			comics: data.comics,
+			series: data.series,
+			error: data.error,
+		};
+	};
+
+	useEffect(() => {
+		if (data.characters) {
+			setCharactersData(data.characters.data);
+			setPageCount(data.characters.data.offset);
+		}
+	});
 
 	return (
 		<Box minHeight={'90vh'}>
-			{error.code === 0 ? (
-				characters != null ? (
-					error.code === 0 ? (
-						<SimpleGrid
-							columns={{ base: 1, md: 2, lg:4 ,xl:5 }}
-							spacing={4}
-							justifyContent="center"
-							justifyItems={'center'}
-							padding={4}
-						>
-							{characters.data.results.map((character) => (
-								<CharacterCards key={character.id} character={character} />
-							))}
-						</SimpleGrid>
+			{data.error.code === 0 ? (
+				data.characters && charactersData ? (
+					data.error.code === 0 ? (
+						<>
+							<SimpleGrid
+								columns={{ base: 1, md: 2, lg: 3, xl: 4 }}
+								spacing={4}
+								justifyContent="center"
+								justifyItems={'center'}
+								padding={4}
+							>
+								{data.characters.data.results.map((character) => (
+									<CharacterCards key={character.id} character={character} />
+								))}
+							</SimpleGrid>
+							<Flex className={styles.paginationButtons}>
+								{charactersData.offset >= 20 ? (
+									<Box
+										fontSize={'3xl'}
+										onClick={async () =>
+											await handleClick(pageCount - numberOfCharactersInPage)
+										}
+									>
+										<Flex className={styles.button}>
+											<ArrowLeftIcon alignSelf={'center'} />
+											Previous
+										</Flex>
+									</Box>
+								) : null}
+								<Box
+									fontSize={'3xl'}
+									onClick={async () =>
+										await handleClick(pageCount + numberOfCharactersInPage)
+									}
+								>
+									<Flex className={styles.button}>
+										Next
+										<ArrowRightIcon alignSelf={'center'} />
+									</Flex>
+								</Box>
+							</Flex>
+						</>
 					) : (
-						<ErrorAlert message={characters.status} />
+						<ErrorAlert message={data.characters.status} />
 					)
 				) : (
 					<Spinner />
 				)
 			) : (
-				<ErrorAlert message={error.message} />
+				<ErrorAlert message={data.error.message} />
 			)}
 		</Box>
 	);
